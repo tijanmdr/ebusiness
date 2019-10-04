44c168bb9cc0fdfb08ee1695a840bdf86b53de02
Tijan Manandhar <tijanmdr@gmail.com>
2019-10-04 17:12:30 +1000

    models for car done

---
+++ src/java/Car/NewCar.java
@@ -0,0 +1,111 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Car;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ *
+ * @author Tijan
+ */
+@Entity
+public class NewCar extends Car implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String warranty;
+    private String extWarranty;
+    private String rsap;
+    private String stockCars;
+
+    public NewCar(Long id, String warranty, String extWarranty, String rsap, String stockCars) {
+        this.id = id;
+        this.warranty = warranty;
+        this.extWarranty = extWarranty;
+        this.rsap = rsap;
+        this.stockCars = stockCars;
+    }
+
+    public NewCar(Long id, String warranty, String extWarranty, String rsap, String stockCars, Long mainCarID, Long price, String refNum, String make, String model, String type, String color, String transmission, String engine, String fuel, int doors, int seats) {
+        super(mainCarID, price, refNum, make, model, type, color, transmission, engine, fuel, doors, seats);
+        this.id = id;
+        this.warranty = warranty;
+        this.extWarranty = extWarranty;
+        this.rsap = rsap;
+        this.stockCars = stockCars;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getWarranty() {
+        return warranty;
+    }
+
+    public void setWarranty(String warranty) {
+        this.warranty = warranty;
+    }
+
+    public String getExtWarranty() {
+        return extWarranty;
+    }
+
+    public void setExtWarranty(String extWarranty) {
+        this.extWarranty = extWarranty;
+    }
+
+    public String getRsap() {
+        return rsap;
+    }
+
+    public void setRsap(String rsap) {
+        this.rsap = rsap;
+    }
+
+    public String getStockCars() {
+        return stockCars;
+    }
+
+    public void setStockCars(String stockCars) {
+        this.stockCars = stockCars;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof NewCar)) {
+            return false;
+        }
+        NewCar other = (NewCar) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Car.NewCar[ id=" + id + " ]";
+    }
+    
+}
---
+++ src/java/Car/OldCar.java
@@ -0,0 +1,134 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Car;
+
+import java.io.Serializable;
+import java.util.Date;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ *
+ * @author Tijan
+ */
+@Entity
+public class OldCar extends Car implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private Long odometer;
+    private int regoNum;
+    private String regoExp;
+    private String serviceHistory;
+    private String vehicleID;
+    private String carHistory;
+
+    public OldCar(Long id, Long odometer, int regoNum, String regoExp, String serviceHistory, String vehicleID, String carHistory) {
+        this.id = id;
+        this.odometer = odometer;
+        this.regoNum = regoNum;
+        this.regoExp = regoExp;
+        this.serviceHistory = serviceHistory;
+        this.vehicleID = vehicleID;
+        this.carHistory = carHistory;
+    }
+    
+    public OldCar(Long id, Long odometer, int regoNum, String regoExp, String serviceHistory, String vehicleID, String carHistory, Long mainCarID, Long price, String refNum, String make, String model, String type, String color, String transmission, String engine, String fuel, int doors, int seats) {
+        super(mainCarID, price, refNum, make, model, type, color, transmission, engine, fuel, doors, seats);
+        this.id = id;
+        this.odometer = odometer;
+        this.regoNum = regoNum;
+        this.regoExp = regoExp;
+        this.serviceHistory = serviceHistory;
+        this.vehicleID = vehicleID;
+        this.carHistory = carHistory;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getOdometer() {
+        return odometer;
+    }
+
+    public void setOdometer(Long odometer) {
+        this.odometer = odometer;
+    }
+
+    public int getRegoNum() {
+        return regoNum;
+    }
+
+    public void setRegoNum(int regoNum) {
+        this.regoNum = regoNum;
+    }
+
+    public String getRegoExp() {
+        return regoExp;
+    }
+
+    public void setRegoExp(String regoExp) {
+        this.regoExp = regoExp;
+    }
+
+    public String getServiceHistory() {
+        return serviceHistory;
+    }
+
+    public void setServiceHistory(String serviceHistory) {
+        this.serviceHistory = serviceHistory;
+    }
+
+    public String getVehicleID() {
+        return vehicleID;
+    }
+
+    public void setVehicleID(String vehicleID) {
+        this.vehicleID = vehicleID;
+    }
+
+    public String getCarHistory() {
+        return carHistory;
+    }
+
+    public void setCarHistory(String carHistory) {
+        this.carHistory = carHistory;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof OldCar)) {
+            return false;
+        }
+        OldCar other = (OldCar) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Car.oldCar[ id=" + id + " ]";
+    }
+    
+}
